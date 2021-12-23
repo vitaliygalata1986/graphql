dcf3abc77c64a0e7335e514ceea36bcdbaffc768
Vitaliy Galata <nertis44@gmail.com>
2021-12-23 12:28:34 +0300

    add js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const { join } = require('path')
+const path = require('path')
 const app = express() // создаем объект appication, вызвав функцию express
 const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use((req,res,next)=>{
+    res.sendFile('/index.html')
+})
+
 
 app.listen(PORT)
